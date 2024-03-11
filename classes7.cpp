a891cc0812a771a79ed5a4190b0c11acce8252e9
Siddharth Chauhan <siddharthc443@gmail.com>
2024-03-11 22:08:33 +0530

    first commmit

--- classes7.cpp
+++ classes7.cpp
@@ -1,11 +1,35 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
 
+class combine{
+    private:
+    char str1[20];
+    char str2[20];
 
+    public:
+    void GetData(){
+        cout<<"Enter first string :";
+        cin>>str1;
+
+        cout<<"Enter secind string";
+        cin>>str2;
+    }
+        void display()
+        {
+            strcat(str1,str2);
+            cout<<"\nThe combination of the strings are :"<<str1;
+        }
+
+
+};
 
 int main()
-{
-    
+{   combine obj1;
+        obj1.GetData();
+        obj1.display();
+        return 0;
+
 
     return 0;
 }
